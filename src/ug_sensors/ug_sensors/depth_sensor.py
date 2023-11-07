fe31a962d48b6dc2c40128d5d38a9d94a7b3bd23
Anthony Cieri <penguinmillion@gmail.com>
2023-11-06 19:59:17 -0800

    feat: depthSensor class, communicates with sensor driver

--- src/ug_sensors/ug_sensors/depth_sensor.py
+++ src/ug_sensors/ug_sensors/depth_sensor.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 """
-    Originally written by Anthony Cieri penguinmillion@gmail.com
+    Originally written by:
+        Anthony Cieri penguinmillion@gmail.com
 
-    This creates a ros2 node that reads from the baromatric
+    This creates a ros2 node that reads from the depth
     sensor and publishes the readings to the sensor topic.
 
     In its current state, this code can be used as a base for any
@@ -11,20 +12,25 @@
 
     DONE: Create a ros2 node that publishes data to a topic.
 
-    TODO: Develop the sensor topic with Rowan.
-    TODO: Decide what types will be published to the topic.
+    TODO: Develop the sensor interface with David.
 
-    TODO: Get data from the sensor, requires appropriate libs
+    DONE: Get data from the sensor, requires appropriate libs
             and hardware (raspberry pi and sensor).
+
+    TODO: Should a separate node be created for temperature?
 """
 
-import math  # May be removed later
 import rclpy  # For ros2
 from rclpy.node import Node  # To create the node
-from std_msgs.msg import Float32  # TODO: Decide this type
+from std_msgs.msg import (
+    Float32 as depth_sensor_interface,
+)  # TODO: Create topic interface with David
 
-# TODO: Import appropriate modules to interface with the sensor
-# TODO: Import appropriate modules for any processing of raw data
+# Sensor modules
+# This should be a module between the actual sensor driver and this script that
+# Translates whatever the sensor driver gives to what this script expects
+# TODO: What does this script expect? (Specify abstract sensor)
+import sensor.ms5837_sensor as sensor_driver
 
 
 """
@@ -40,9 +46,9 @@ def main(args=None):
 
 
 """
-    Currently all this does is publish arbitrary floats to a topic.
+    Node that periodically publishes depth sensor data to the /sensors/depth topic
 
-    TODO: Publish data from sensor
+    TODO: Error handling
 """
 
 
@@ -50,11 +56,12 @@ class depthSensorNode(Node):
     def __init__(self):
         super().__init__("depth_sensor")
 
-        # TODO: For demonstration only, replace with real data
-        self.data_ = 5.598327
+        self.depth_sensor = depthSensor(fresh_water=False)
 
         # Create publisher for data
-        self.depth_data_pub_ = self.create_publisher(Float32, "/sensors/depth", 10)
+        self.depth_data_pub_ = self.create_publisher(
+            depth_sensor_interface, "/sensors/depth", 10
+        )
 
         # Every half second publish data to the topic
         # TODO: How often does this need to be updated?
@@ -64,41 +71,74 @@ class depthSensorNode(Node):
         # TODO: Standardize log messages with other team members
         self.get_logger().info("Depth sensor node started")
 
-    # TODO: Need a method for reading data from sensor
-
     # Publish the data
-    # TODO: Rename to something more clear
     def publish_depth_data(self):
-        # TODO: Change type
-        msg = Float32()
+        msg = depth_sensor_interface()
 
-        # TODO: Replace this variable with a method call
-        msg.data = self.data_
+        msg.data = self.depth_sensor.read_depth()
 
         # Publish data
-        # TODO: Format data
         self.depth_data_pub_.publish(msg)
 
         # Log data (Debugging)
-        # TODO: Remove this, creates noise in log
-        self.get_logger().info("Hello " + str(self.data_))
-
-        # TODO: For demonstration only
-        self.data_ /= self.data_ + math.modf(self.data_)[1]
+        # self.get_logger().info("Hello " + str(self.data_))
 
 
 """
-    TODO: Create a class to manage sensor data.
-            Needs a method to read from sensor
-            One to process data
-            One to format data
-            Maybe one to report status of sensor?
+    Simple class to manage the sensor driver and provide some abstraction
 
+    TODO: Error handling. What happens if __sensor.read() is false?
+
+    __sensor.read() must be called whenever new data is requested,
+    but if both depth and temp are wanted simultaniously, theres no
+    reason to call read() twice.
+"""
+
+
+class depthSensor:
+    # Initalize the physical sensor, returns whether init was successful
+    def __init__(self, fresh_water=True) -> bool:
+        self.__sensor = (
+            sensor_driver.MS5837_30BA()
+        )  # Default I2C bus is 1 (Raspberry Pi 3)
+
+        # We must initialize the sensor before reading it
+        if not self.__sensor.init():
+            # Sensor could not be initialized
+            return False
+
+        # We have to read values from sensor to update pressure and temperature
+        if not self.__sensor.read():
+            # Sensor read failed!
+            return False
+
+        # Freshwater is default, but the glider would usually be in saltwater
+        if fresh_water:
+            self.__sensor.setFluidDensity(sensor_driver.DENSITY_FRESHWATER)
+        else:
+            self.__sensor.setFluidDensity(sensor_driver.DENSITY_SALTWATER)
+
+        return True
+
+    # Only call read() once for all data
+    def read_depth_temp(self) -> tuple[float, float]:
+        if self.__sensor.read():
+            return (self.__sensor.depth(), self.__sensor.temp())
+
+    # Read the depth in mBar from the sensor
+    def read_depth(self) -> float:
+        if self.__sensor.read():
+            return self.__sensor.depth()
+
+    # Read the temperature in Celcius from the sensor
+    def read_temp(self) -> float:
+        if self.__sensor.read():
+            return self.__sensor.temperature()
+
+
+"""
     TODO: How much processing needs to be done?
-            Does it publish raw data?
-            Calibrated?
-            What units?
-            Multiple streams of data?
+            Multiple streams of data? Depends on the topic interface
             The sensor can give temp as well as pressure,
-                does it need to publish that as well?
+                does it need to publish that as well? Yes.
 """
