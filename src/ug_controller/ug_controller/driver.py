fc221b9494b019fa4e28333562b1f335197f6fe5
Philip Esclamado <philipesclamado@uvic.ca>
2023-11-06 23:00:38 -0800

    chore: outline the driver loop

--- src/ug_controller/ug_controller/driver.py
+++ src/ug_controller/ug_controller/driver.py
@@ -1,15 +1,2 @@
 #!/usr/bin/env python3
-import rclpy
-from rclpy.node import Node
-
-class DriverSubscriberNode(Node):
-    def __init__(self):
-        super().__init__("driver_subscriber")
-
-def main(args=None):
-    rclpy.init(args=None)
-
-    rclpy.shutdown()
-
-if __name__ == "__main__":
-    main()
+"""driver loop logic"""
