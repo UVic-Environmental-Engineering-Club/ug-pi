2928182113e7fdbdf8c71b682a208138fafb4b27
Anthony Cieri <penguinmillion@gmail.com>
2024-07-10 20:19:31 -0700

    fix: incorrect paths

--- src/pi_startup/auto_update_service/setup.sh
+++ src/pi_startup/auto_update_service/setup.sh
@@ -4,14 +4,19 @@
 # Test script
 # Runs some other scripts
 
-git_dir='/home/pi/Documents/ug-pi/'
+git_dir='/home/pi/Documents/ug-pi'
+log_path='~/Documents/setup.log'
 
-cd git_dir
+cd $git_dir
 
-git pull >> ~/Documents/setup.log
+# New line
+echo >> $log_path
+date >> $log_path
+
+git pull >> $log_path
 
 echo $(pwd) > testing.txt
 
-bash ./src/pi_startup/startup_stuff/test.sh
+bash $git_dir/src/pi_startup/startup_stuff/test.sh
 
-python3 ./src/pi_startup/startup_stuff/test.py > ~/Documents/test_py_output.txt
+python3 $git_dir/src/pi_startup/startup_stuff/test.py > ~/Documents/test_py_output.txt
